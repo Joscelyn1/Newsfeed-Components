4ca7599c664ec37ecd2e5652acb481adae0020cd
Joscelyn <JoscelynRose@gmail.com>
2019-06-19 15:40:46 -0400

    added toggle feature to menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -8,10 +9,9 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 
 
-const menuButton = document.querySelectorAll('.menu-button')
+const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.forEach((button) => {
-  button.addEventListener('click', function(event) {
-    
-  })
+
+menuButton.addEventListener('click', function(event) {
+  toggleMenu();
 })
